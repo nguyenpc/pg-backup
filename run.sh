fd98972bbb7044b6e09bc355eaa945ce2b05f4f7
Lukasz Karolewski <lukasz-karolewski@users.noreply.github.com>
2018-08-17 23:07:36 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,14 +3,18 @@
 ./backup.sh
 
 if [ -n "$CRON_TIME" ]; then
+  CRON_FILE=/etc/cron.d/pg-dockup
+
   # force set environment vars 
   env > /etc/environment
   
   # configure cron
-  echo "$CRON_TIME root $DIR/backup.sh >> /var/log/cron.log 2>&1" > /etc/cron.d/pg-dockup
-  chmod 0644 /etc/cron.d/pg-dockup
+  echo "$CRON_TIME root $DIR/backup.sh >> /var/log/cron.log 2>&1" > $CRON_FILE
+  echo "# Don't remove the empty line at the end of this file. It is required to run the cron job" >> $CRON_FILE
+  
+  chmod 0644 $CRON_FILE
   touch /var/log/cron.log
 
-  echo "=> Running dockup backups with following crontab $CRON_TIME"
+  echo "=> Backup running with following schedule $CRON_TIME"
   cron && tail -f /var/log/cron.log
 fi
