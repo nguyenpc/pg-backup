aaaaa7411dc34e1cf713b5ae7446d6a8eeea738a
Lukasz Karolewski <lkarolewski@gmail.com>
2018-11-29 13:16:27 -0800

    updated filename format and keeping backups from last 30 days

--- backup.sh
+++ backup.sh
@@ -3,15 +3,15 @@ echo "Starting backup"
 export PATH=$PATH:/usr/bin:/usr/local/bin:/bin
 
 # Generate filenames
-readonly LOCAL_BACKUP=$LOCAL_BACKUP_DIR/$BACKUP_NAME-$(date +%A).gzip
-readonly REMOTE_BACKUP=$BACKUP_NAME-$(date +"%Y-%m-%dT%H:%M:%SZ").gzip
+readonly $BACKUP_FILENAME=$BACKUP_NAME-$(date +"%Y-%m-%dT%H:%M:%SZ").gzip
+readonly LOCAL_BACKUP_PATH=$LOCAL_BACKUP_DIR/$BACKUP_FILENAME
 
 # Run backup
-pg_dump --dbname=$PG_CONNECTION_STRING $PG_DUMP_OPTIONS | gzip > $BACKUP_FILENAME
+pg_dump --dbname=$PG_CONNECTION_STRING $PG_DUMP_OPTIONS | gzip > $LOCAL_BACKUP_PATH
 
 # Keep backups from last 30 days locally
 find . -type f -name '*.gzip' -mtime +30 -delete
 
 # Upload the backup to S3
-aws s3 --region $AWS_DEFAULT_REGION cp $BACKUP_FILENAME s3://$AWS_S3_BUCKET_NAME/$BACKUP_FILENAME $AWS_S3_CP_OPTIONS
+aws s3 --region $AWS_DEFAULT_REGION cp $LOCAL_BACKUP_PATH s3://$AWS_S3_BUCKET_NAME/$BACKUP_FILENAME $AWS_S3_CP_OPTIONS
 echo "backup done"
