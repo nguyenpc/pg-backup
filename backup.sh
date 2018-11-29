7c6aae149ae947ee62c04ea89b5ad2337512e0a2
Lukasz Karolewski <lkarolewski@gmail.com>
2018-11-29 13:15:00 -0800

    updated filename format and keeping backups from last 30 days

--- backup.sh
+++ backup.sh
@@ -7,8 +7,11 @@ readonly LOCAL_BACKUP=$LOCAL_BACKUP_DIR/$BACKUP_NAME-$(date +%A).gzip
 readonly REMOTE_BACKUP=$BACKUP_NAME-$(date +"%Y-%m-%dT%H:%M:%SZ").gzip
 
 # Run backup
-pg_dump --dbname=$PG_CONNECTION_STRING $PG_DUMP_OPTIONS | gzip > $LOCAL_BACKUP
+pg_dump --dbname=$PG_CONNECTION_STRING $PG_DUMP_OPTIONS | gzip > $BACKUP_FILENAME
+
+# Keep backups from last 30 days locally
+find . -type f -name '*.gzip' -mtime +30 -delete
 
 # Upload the backup to S3
-aws s3 --region $AWS_DEFAULT_REGION cp $LOCAL_BACKUP s3://$AWS_S3_BUCKET_NAME/$REMOTE_BACKUP $AWS_S3_CP_OPTIONS
+aws s3 --region $AWS_DEFAULT_REGION cp $BACKUP_FILENAME s3://$AWS_S3_BUCKET_NAME/$BACKUP_FILENAME $AWS_S3_CP_OPTIONS
 echo "backup done"
