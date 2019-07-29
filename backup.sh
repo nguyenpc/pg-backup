da6aae981a4f5d28602407f8f7d1c181ddc22f30
Lukasz Karolewski <lukasz-karolewski@users.noreply.github.com>
2019-07-28 20:25:29 -0700

    prevent uploading empty backups

--- backup.sh
+++ backup.sh
@@ -6,15 +6,25 @@ echo "Starting backup"
 export PATH=$PATH:/usr/bin:/usr/local/bin:/bin
 
 # Generate filenames
-readonly BACKUP_FILENAME=$BACKUP_NAME-$(date +"%Y-%m-%dT%H-%M-%SZ").gzip
+readonly BACKUP_FILENAME=$BACKUP_NAME-$(date +"%Y-%m-%dT%H-%M-%SZ")
 readonly LOCAL_BACKUP_PATH=$LOCAL_BACKUP_DIR/$BACKUP_FILENAME
 
 # Run backup
-pg_dump --dbname=$PG_CONNECTION_STRING $PG_DUMP_OPTIONS | gzip > $LOCAL_BACKUP_PATH
+pg_dump --dbname=$PG_CONNECTION_STRING $PG_DUMP_OPTIONS > $LOCAL_BACKUP_PATH
 
-# Keep backups from last 30 days locally
+# check if not empty
+if [ ! -s $LOCAL_BACKUP_PATH ]; then 
+  rm -f $LOCAL_BACKUP_PATH
+  echo "empty backup, exiting"
+  exit 1
+fi
+
+cat $LOCAL_BACKUP_PATH | gzip > $LOCAL_BACKUP_PATH.gzip
+
+# Remove uncompressed pg_dump and prune local gzipped backups older than 30 days 
+rm -f $LOCAL_BACKUP_PATH
 find . -type f -name '*.gzip' -mtime +30 -delete
 
 # Upload the backup to S3
-aws s3 --region $AWS_DEFAULT_REGION cp $LOCAL_BACKUP_PATH s3://$AWS_S3_BUCKET_NAME/$BACKUP_FILENAME $AWS_S3_CP_OPTIONS
+aws s3 --region $AWS_DEFAULT_REGION cp $LOCAL_BACKUP_PATH.gzip s3://$AWS_S3_BUCKET_NAME/$BACKUP_FILENAME.gzip $AWS_S3_CP_OPTIONS
 echo "backup done"
